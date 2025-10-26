dd0d386908649b46d2f0ed955a1bf95b37115d61
lostmart <martinpedraza1979@gmail.com>
2025-10-26 13:25:33 +0100

    start app express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("here we go !!")
+import express from "express"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+	res.json({ message: "Hello, World!" })
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
